754b17b31bcf2e0433c5c36c1af2e42950f8d2e8
cameron.cinel <cameron.cinel@kitware.com>
2025-08-08 16:38:25 -0400

    Updating dataframe fields for use with JHU easier

--- magnet/loaders.py
+++ magnet/loaders.py
@@ -18,31 +18,39 @@ def load_run_spec(run_spec_file_path):
 
 
 def load_scenario_state(suite, run_spec, root_dir="benchmark_output"):
-    state_file_path = os.path.join(root_dir, 'runs', suite, run_spec, "scenario_state.json")
+    state_file_path = os.path.join(
+        root_dir, "runs", suite, run_spec, "scenario_state.json"
+    )
     with open(state_file_path) as f:
         json_state: Dict[str, Any] = json.load(f)
         scenario_state = dacite.from_dict(ScenarioState, json_state)
 
     return scenario_state
 
-def load_all_scenario_states_as_dataframe(suite, run_specs, root_dir="benchmark_output"):
+
+def load_all_scenario_states_as_dataframe(
+    suite, run_specs, root_dir="benchmark_output"
+):
     scenario_states_records = []
     for run_spec in run_specs:
-        state = load_scenario_state(suite, run_spec, root_dir=root_dir)
+        state: ScenarioState = load_scenario_state(suite, run_spec, root_dir=root_dir)
+        model = state.adapter_spec.model.replace(os.path.sep, "_")
 
         for request_state in state.request_states:
             input = request_state.instance.input.text
             output = request_state.result.completions[0].text
 
-            scenario_states_records.append((run_spec, input, output))
+            scenario_states_records.append((run_spec, model, input, output))
 
-    scenario_states_df = pd.DataFrame(scenario_states_records, columns=["run_spec", "input", "output"])
+    scenario_states_df = pd.DataFrame(
+        scenario_states_records, columns=["run_spec", "model", "input", "output"]
+    )
 
     return scenario_states_df
 
 
 def load_stats(suite, run_spec, root_dir="benchmark_output"):
-    stats_file_path = os.path.join(root_dir, 'runs', suite, run_spec, "stats.json")
+    stats_file_path = os.path.join(root_dir, "runs", suite, run_spec, "stats.json")
     with open(stats_file_path) as f:
         json_stats: List[Dict[str, Any]] = json.load(f)
         stats = [dacite.from_dict(Stat, json_stat) for json_stat in json_stats]
@@ -57,49 +65,65 @@ def load_all_stats_as_dataframe(suite, run_specs, root_dir="benchmark_output"):
 
         for stat in stats:
             if stat.name.perturbation is None:
-                stats_records.append((run_spec,
-                                      stat.name.name,
-                                      None,None,None,None,None,  # perturbation name, robustness, fairness, computed_on, seed
-                                      stat.count,
-                                      stat.sum,
-                                      stat.sum_squared,
-                                      stat.min,
-                                      stat.max,
-                                      stat.mean,
-                                      stat.variance,
-                                      stat.stddev))
+                stats_records.append(
+                    (
+                        run_spec,
+                        stat.name.name,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,  # perturbation name, robustness, fairness, computed_on, seed
+                        stat.count,
+                        stat.sum,
+                        stat.sum_squared,
+                        stat.min,
+                        stat.max,
+                        stat.mean,
+                        stat.variance,
+                        stat.stddev,
+                    )
+                )
             else:
-                stats_records.append((run_spec,
-                                      stat.name.name,
-                                      stat.name.perturbation.name,
-                                      stat.name.perturbation.robustness,
-                                      stat.name.perturbation.fairness,
-                                      stat.name.perturbation.computed_on,
-                                      stat.name.perturbation.seed,
-                                      stat.count,
-                                      stat.sum,
-                                      stat.sum_squared,
-                                      stat.min,
-                                      stat.max,
-                                      stat.mean,
-                                      stat.variance,
-                                      stat.stddev))
+                stats_records.append(
+                    (
+                        run_spec,
+                        stat.name.name,
+                        stat.name.perturbation.name,
+                        stat.name.perturbation.robustness,
+                        stat.name.perturbation.fairness,
+                        stat.name.perturbation.computed_on,
+                        stat.name.perturbation.seed,
+                        stat.count,
+                        stat.sum,
+                        stat.sum_squared,
+                        stat.min,
+                        stat.max,
+                        stat.mean,
+                        stat.variance,
+                        stat.stddev,
+                    )
+                )
 
-    stats_df = pd.DataFrame(stats_records,
-                            columns=["run_spec",
-                                     "name",
-                                     "perturbation_name",
-                                     "perturbation_robustness",
-                                     "perturbation_fairness",
-                                     "perturbation_computed_on",
-                                     "perturbation_seed",
-                                     "count",
-                                     "sum",
-                                     "sum_squared",
-                                     "min",
-                                     "max",
-                                     "mean",
-                                     "variance",
-                                     "stddev"])
+    stats_df = pd.DataFrame(
+        stats_records,
+        columns=[
+            "run_spec",
+            "name",
+            "perturbation_name",
+            "perturbation_robustness",
+            "perturbation_fairness",
+            "perturbation_computed_on",
+            "perturbation_seed",
+            "count",
+            "sum",
+            "sum_squared",
+            "min",
+            "max",
+            "mean",
+            "variance",
+            "stddev",
+        ],
+    )
 
     return stats_df
--- magnet/predictor.py
+++ magnet/predictor.py
@@ -9,14 +9,12 @@ from helm.benchmark.metrics.statistic import Stat
 from magnet.loaders import (
     load_run_spec,
     load_all_scenario_states_as_dataframe,
-    load_all_stats_as_dataframe)
+    load_all_stats_as_dataframe,
+)
 
 
 class Predictor:
-    def __init__(self,
-                 num_example_runs=3,
-                 num_eval_samples=20,
-                 random_seed=1):
+    def __init__(self, num_example_runs=3, num_eval_samples=20, random_seed=1):
         self.num_example_runs = num_example_runs
         self.num_eval_samples = num_eval_samples
         self.random_seed = random_seed
@@ -29,10 +27,9 @@ class Predictor:
         # To be overridden
         return True
 
-    def predict(self,
-                train_scenario_states_df,
-                train_stats_df,
-                eval_scenario_states_df) -> List[Stat]:
+    def predict(
+        self, train_scenario_states_df, train_stats_df, eval_scenario_states_df
+    ) -> List[Stat]:
         raise NotImplementedError
 
     def __call__(self, root_dir, suite):
@@ -40,30 +37,40 @@ class Predictor:
             random.seed(self.random_seed)
 
         selected_run_specs = []
-        for run_spec_fp in glob(os.path.join(root_dir, 'runs', suite, '*', 'run_spec.json')):
+        for run_spec_fp in glob(
+            os.path.join(root_dir, "runs", suite, "*", "run_spec.json")
+        ):
             run_spec = load_run_spec(run_spec_fp)
 
             if self.run_spec_filter(run_spec):
                 selected_run_specs.append(run_spec)
 
-        *train_runs, eval_run = random.sample(selected_run_specs, self.num_example_runs + 1)
+        *train_runs, eval_run = random.sample(
+            selected_run_specs, self.num_example_runs + 1
+        )
 
         train_scenario_states_df = load_all_scenario_states_as_dataframe(
-            suite, [r.name for r in train_runs], root_dir=root_dir)
+            suite, [r.name for r in train_runs], root_dir=root_dir
+        )
 
         train_stats_df = load_all_stats_as_dataframe(
-            suite, [r.name for r in train_runs], root_dir=root_dir)
+            suite, [r.name for r in train_runs], root_dir=root_dir
+        )
 
         _full_eval_scenario_states_df = load_all_scenario_states_as_dataframe(
-            suite, [eval_run.name], root_dir=root_dir)
+            suite, [eval_run.name], root_dir=root_dir
+        )
 
         random_eval_indices = random.sample(
-            range(len(_full_eval_scenario_states_df)), self.num_eval_samples)
-        eval_scenario_states_df = _full_eval_scenario_states_df.iloc[random_eval_indices]
+            range(len(_full_eval_scenario_states_df)), self.num_eval_samples
+        )
+        eval_scenario_states_df = _full_eval_scenario_states_df.iloc[
+            random_eval_indices
+        ]
 
-        predicted_stats = self.predict(train_scenario_states_df,
-                                       train_stats_df,
-                                       eval_scenario_states_df)
+        predicted_stats = self.predict(
+            train_scenario_states_df, train_stats_df, eval_scenario_states_df
+        )
 
         # TODO: Do something meaningful with the predictions
         print(predicted_stats)
