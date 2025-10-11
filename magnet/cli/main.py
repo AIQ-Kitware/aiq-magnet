e3e673d925ae6d5dd17af8dbaca209220890a4ce
joncrall <jon.crall@kitware.com>
2025-10-11 15:12:29 -0400

    add CLI test

--- magnet/cli/main.py
+++ magnet/cli/main.py
@@ -1,34 +1,28 @@
 """
 The top level Magnet CLI
+
+Example:
+    >>> # Test that help works for each subcli
+    >>> from magnet.cli.main import *  # NOQA
+    >>> MagnetCLI.main(argv=['--help'], _noexit=True)
+    >>> MagnetCLI.main(argv=['download', '--help'], _noexit=True)
+    >>> MagnetCLI.main(argv=['download', 'helm', '--help'], _noexit=True)
+    >>> # Test version works
+    >>> MagnetCLI.main(argv=['--version'])
 """
 import scriptconfig as scfg
 from magnet.cli.download_cli import DownloadModalCLI
 from magnet import __version__
-
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument('foo', default='1', required=False)
-# PortedParser = scfg.DataConfig.cls_from_argparse(parser, name='blagh')
-
-
-# def main(cmdline=None, **kw):
-#     args = PortedParser.cli(cmdline=cmdline, data=kw, strict=True, special_options=False)
-
-# PortedParser.main = main
-# parser.parse_args()
 
 
 class MagnetCLI(scfg.ModalCLI):
     """
     Top level magnet modal CLI
     """
-    # __version__ = __version__
-    ...
+    __version__ = __version__
 
 
-# MagnetCLI.register(DownloadModalCLI)
 MagnetCLI.register(DownloadModalCLI, command='download')
-# MagnetCLI.register(PortedParser, command='blagh')
 
 
 __cli__ = MagnetCLI
