6be27ac7b04a07015e12200533807d2d5eee4427
Benjamin Fenelon <benjamin.fenelon@kitware.com>
2025-11-20 16:11:17 -0500

    Add sweep to symbol definitions

--- magnet/evaluation.py
+++ magnet/evaluation.py
@@ -1,12 +1,10 @@
 import builtins
 from graphlib import TopologicalSorter
+from itertools import product
 from typing import Any, Dict, List, Tuple, get_origin, get_args
 import yaml
 import argparse
 
-
-def _check_arbitrary_code(code_str: str) -> None:
-    pass
 
 class EvaluationCard:
     """
@@ -31,7 +29,7 @@ class EvaluationCard:
         self.claim = Claim(cfg.get("claim"))
         self.symbols = Symbols(cfg.get("symbols", {}))
 
-    def evaluate(self) -> str:
+    def evaluate(self) -> tuple:
         """
         Run the evaluation specification
 
@@ -40,8 +38,25 @@ class EvaluationCard:
         """
         # Could log requests from here (i.e. timestamps), I think this was done in other code segments
         self.symbols.resolve()
-        self.claim.evaluate(self.symbols())
-        return self.claim.status
+
+        aggregate_results = []
+        for symbol_assignment in self.symbols():
+            # FIXME: propagate failure parameters
+            results, _ = self.claim.evaluate(symbol_assignment)
+
+            aggregate_results.append(results)
+
+        total = len(self.symbols())
+        percentage = lambda count: count / total
+
+        print("================================")
+        print(f"Settings Evaluated: {total}")
+        print(f"  Verified:     {percentage(aggregate_results.count('VERIFIED')):.2f}")
+        print(f"  Falsified:    {percentage(aggregate_results.count('FALSIFIED')):.2f}")
+        print(f"  Inconclusive: {percentage(aggregate_results.count('INCONCLUSIVE')):.2f}")
+        print("================================")
+
+        return self.claim.status #, aggregate_results
 
     def summarize(self):
         """
@@ -53,7 +68,7 @@ class EvaluationCard:
         print(f"SYMBOLS:     {self.symbols()}")
         print(f"CLAIM:       \n{self.claim}")
         print("================================")
-        print(f"STATUS:      {self.claim.status}""")
+        print(f"STATUS:      {self.claim.status}""") # TODO: Fix for parameter sweeps
 
 class Claim:
     """
@@ -104,6 +119,8 @@ class Claim:
         except Exception as e:
             self.status = "INCONCLUSIVE"
             print(f"ERROR evaluating claim: {e}")
+        finally:
+            return self.status, symbols
 
     def __repr__(self) -> str:
         return self.claim
@@ -121,6 +138,7 @@ class Symbol:
     def __init__(self, name, spec):
         self.name = name
         self.value = spec.get('value')
+        self.sweep = spec.get('sweep')
         self.type = spec.get('type', 'List[int]')
         self.definition = spec.get('python', '')
         self.dependencies = spec.get('depends_on', [])
@@ -176,40 +194,70 @@ class Symbol:
 
 class Symbols:
     """
-    Dictionary of Symbols used as context for claim
+    Collection of Symbol configurations used as context for claim
 
     Example:
         >>> from magnet.evaluation import Symbols
         >>> symbols = Symbols({'x': {'type': "List[int]", 'python': "x = [10]"}})
         >>> symbols()
-        {'x': None}
+        [{'x': None}]
         >>> symbols.resolve()
         >>> symbols()
-        {'x': [10]}
+        [{'x': [10]}]
     """
     def __init__(self, symbol_specs) -> None:
-        self.symbols = {symbol: Symbol(symbol, definition) for symbol, definition in symbol_specs.items()}
+        self.symbols = self._decompose_symbol_defs(symbol_specs)
 
+    def _decompose_symbol_defs(self, symbol_definitions) -> List[Dict[str, Symbol]]:
+        """
+        Flatten sweep values into unique combinations of symbol values
+        """
+        configurations = []
+        aggregate_configuration = {symbol: Symbol(symbol, definition) for symbol, definition in symbol_definitions.items()}
+
+        sweep_symbols = self._find_sweep_symbols(aggregate_configuration)
+        if sweep_symbols: 
+            sweep_values = [sweep.sweep for sweep in sweep_symbols]
+            combinations = product(*sweep_values)
+            
+            from copy import deepcopy
+            for combo in combinations:
+                # TODO: look into if product can maintain some naming
+                sweep_fill = dict(zip([symbol.name for symbol in sweep_symbols], combo))
+                editable = deepcopy(aggregate_configuration)
+                for k,v in sweep_fill.items():
+                    editable[k].value = v
+                configurations.append(editable)
+        else:
+            configurations.append(aggregate_configuration)
+
+        return configurations
+    
     def resolve(self):
         """
         Trace dependency graph to resolve each symbol definition
 
         Values stored in Symbol instances
         """
-        symbol_definitions = {}
-        for symbol in self._construct_dependency_order():
-            symbol_definitions[symbol] = self.symbols[symbol].eval(symbol_definitions.copy())
+        for config in self.symbols:
+            symbol_definitions = {}
 
-    def _construct_dependency_order(self) -> List[Symbol]:
+            for symbol in self._construct_dependency_order(config):
+                symbol_definitions[symbol] = config[symbol].eval(symbol_definitions.copy())
+        
+    def _find_sweep_symbols(self, symbol_definitions) -> List[Symbol]:
+        return [symbol for symbol in symbol_definitions.values() if symbol.sweep]
+
+    def _construct_dependency_order(self, configuration) -> List[Symbol]:
         """
         Construct dependency order
         """
-        dependency_graph = {symbol: instance.dependencies for symbol, instance in self.symbols.items()}
+        dependency_graph = {name: symbol.dependencies for name, symbol in configuration.items()}
         sorter = TopologicalSorter(dependency_graph)
         return list(sorter.static_order())
 
     def __call__(self):
-        return {symbol: self.symbols[symbol].value for symbol in self.symbols}
+        return [{symbol: configuration[symbol].value for symbol in configuration} for configuration in self.symbols]
 
 
 def build_parser():
