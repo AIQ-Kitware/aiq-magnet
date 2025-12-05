e1a4aeea2c29b4500956a4160e8fbff649f5e242
joncrall <jon.crall@kitware.com>
2025-12-05 15:36:19 -0500

    add enrichment to test and train split for instances

--- magnet/instance_predictor.py
+++ magnet/instance_predictor.py
@@ -97,47 +97,9 @@ class InstancePredictor(Predictor):
         raise NotImplementedError
 
     def prepare_all_dataframes(self, helm_runs):
-        train_split, test_split = super().prepare_all_dataframes(helm_runs)
-
-        eval_instance_stats_df = test_split.per_instance_stats
-
-        # Add a new dedicated column for easy merging downstream with
-        # "actual" per-instance stats
-        predict_id_col_name = "magnet.instance_predict_id"
-        reindexed_scenario_state = test_split.scenario_state.reset_index(
-            drop=True).reset_index(names=predict_id_col_name)
-        test_split.scenario_state = reindexed_scenario_state
-
-        scenario_state_columns = reindexed_scenario_state.columns.intersection(
-            ['run_spec.name',
-             'scenario_state.request_states.instance.id',
-             'scenario_state.request_states.instance.perturbation.computed_on',
-             'scenario_state.request_states.instance.perturbation.fairness',
-             'scenario_state.request_states.instance.perturbation.name',
-             'scenario_state.request_states.instance.perturbation.prob',
-             'scenario_state.request_states.instance.perturbation.robustness',
-             'scenario_state.request_states.instance.split'])
-
-        per_instance_columns = eval_instance_stats_df.columns.intersection(
-            ['run_spec.name',
-             'per_instance_stats.instance_id',
-             'per_instance_stats.stats.name.perturbation.computed_on',
-             'per_instance_stats.stats.name.perturbation.fairness',
-             'per_instance_stats.stats.name.perturbation.name',
-             'per_instance_stats.stats.name.perturbation.prob',
-             'per_instance_stats.stats.name.perturbation.robustness',
-             'per_instance_stats.stats.name.split'])
-
-        assert len(per_instance_columns) == len(scenario_state_columns)
-
-        merged = pd.merge(eval_instance_stats_df, reindexed_scenario_state,
-                          left_on=list(per_instance_columns),
-                          right_on=list(scenario_state_columns))
-
-        reindexed_eval_instance_stats_df = merged[[predict_id_col_name, *eval_instance_stats_df.columns]]
-
-        test_split.per_instance_stats = reindexed_eval_instance_stats_df
-
+        train_split, test_split = super(InstancePredictor, self).prepare_all_dataframes(helm_runs)
+        for split in [train_split, test_split]:
+            _reindex_split(split)
         return train_split, test_split
 
     def _evaluate(self, helm_runs=None):
@@ -157,4 +119,46 @@ class InstancePredictor(Predictor):
         predicted_instances = self.predict(train_split, sequestered_test_split)
         predicted_instance_stats_df = InstancePrediction.to_df(predicted_instances)
 
-        self.compare_predicted_to_actual(predicted_instance_stats_df, eval_instance_stats_df)
+        return self.compare_predicted_to_actual(predicted_instance_stats_df, eval_instance_stats_df)
+
+
+def _reindex_split(split):
+    # Add a new dedicated column for easy merging downstream with
+    # "actual" per-instance stats
+    predict_id_col_name = "magnet.instance_predict_id"
+
+    instance_stats_df = split.per_instance_stats
+    scenario_state_df = split.scenario_state
+
+    new_scenario_state_df = scenario_state_df.reset_index(drop=True)
+    new_scenario_state_df = new_scenario_state_df.reset_index(names=predict_id_col_name)
+
+    scenario_state_columns = new_scenario_state_df.columns.intersection(
+        ['run_spec.name',
+         'scenario_state.request_states.instance.id',
+         'scenario_state.request_states.instance.perturbation.computed_on',
+         'scenario_state.request_states.instance.perturbation.fairness',
+         'scenario_state.request_states.instance.perturbation.name',
+         'scenario_state.request_states.instance.perturbation.prob',
+         'scenario_state.request_states.instance.perturbation.robustness',
+         'scenario_state.request_states.instance.split'])
+
+    per_instance_columns = instance_stats_df.columns.intersection(
+        ['run_spec.name',
+         'per_instance_stats.instance_id',
+         'per_instance_stats.stats.name.perturbation.computed_on',
+         'per_instance_stats.stats.name.perturbation.fairness',
+         'per_instance_stats.stats.name.perturbation.name',
+         'per_instance_stats.stats.name.perturbation.prob',
+         'per_instance_stats.stats.name.perturbation.robustness',
+         'per_instance_stats.stats.name.split'])
+
+    assert len(per_instance_columns) == len(scenario_state_columns)
+
+    merged = pd.merge(instance_stats_df, new_scenario_state_df,
+                      left_on=list(per_instance_columns),
+                      right_on=list(scenario_state_columns))
+    new_instance_stats_df = merged[[predict_id_col_name, *instance_stats_df.columns]]
+
+    split.scenario_state = new_scenario_state_df
+    split.per_instance_stats = new_instance_stats_df
