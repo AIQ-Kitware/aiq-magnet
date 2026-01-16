50be6d512769534eea2998f973eaaf955fa2b105
David Joy <10147749+dmjoy@users.noreply.github.com>
2026-01-16 16:30:52 -0500

    Add entry_point plugin support for user defined HELM run_specs

--- magnet/backends/helm/magnet_run_helm.py
+++ magnet/backends/helm/magnet_run_helm.py
@@ -1,11 +1,16 @@
+from importlib.metadata import entry_points
 from helm.benchmark.run import main as helm_run_main
 
-# Import our custom run_specs so that they're hooked up for the HELM
-# run
-from magnet.backends.helm import run_specs as _unused
 
+def load_run_specs():
+    # Each entrypoint under the 'helm.run_specs' group should be a
+    # module that includes HELM run_specs (defined using the HELM
+    # run_spec decorator)
+    for ep in entry_points(group='helm.run_specs'):
+        ep.load()
 
 def main():
+    load_run_specs()
     helm_run_main()
 
 if __name__ == "__main__":
